b2b297229508b6e1f441eff74202ee1a1d1c0abd
gil6464 <gil6464@gmail.com>
2021-03-09 20:25:57 +0200

    fixed delete feature

--- app.js
+++ app.js
@@ -2,33 +2,40 @@ const express = require('express');
 const app = express();
 const uuid = require('uuid');
 const morgan = require('morgan');
-const persons = [
-    {name : "gil",
-    number : "0526193164"
+let persons = [
+    {
+    name : "gil",
+    number : "0526193164",
+    id : "0"
     },
     {
     name : "shir N",
-    number : "0506764046"
+    number : "0506764046",
+    id : "1"
     },
     {
     name : "mom",
-    number : "0528772464"
+    number : "0528772464",
+    id : "2"
     },
     {
     name : "shir L",
-    number : "0526412661"
+    number : "0526412661",
+    id : "3"
     },
     {
     name : "dad",
-    number : "0527929533"
+    number : "0527929533",
+    id : "4"
     },
     {
     name : "yair",
-    number : "0542221273"
+    number : "0542221273",
+    id : "5"
     }
 ]
 app.use(express.json())
-app.use(morgan());
+app.use(morgan("tiny"));
 
 app.use(express.static("./build"));
 
@@ -36,16 +43,19 @@ app.get('/api/persons', (req,res) => {
     res.json(persons);
 })
 
-app.delete('/api/persons/:id', async (req,res) => {
-    const id = req.params.id;
-    if(persons[id]) {
-         await persons.splice([id],1);
-        res.send(persons)
+app.delete('/api/persons/:id',(req,res) => {
+    const id = (req.params.id);
+    console.log("id:",id);
+    const listOfId = persons.filter(person => person.id === id);
+    console.log("list of id:",listOfId);
+    if(listOfId.length === 0) {
+        res.status(404).send("no such person");
     } else {
-        res.send("no such person");
+        console.log("else")
+        persons = persons.filter(person => person.id !== id)
+        res.send(persons)
     }
 })
-
 app.get('/api/persons/:id', (req,res) => {
     const id = req.params.id;
     if(persons[id]) {
@@ -65,7 +75,7 @@ app.post('/api/persons',(req,res) => {
         const names = persons.map(person => person.name);
         const checkName = names.filter(name => name === newPerson.name);
         if(checkName.length === 0){
-            newPerson.number = uuid.v4()
+            newPerson.id = uuid.v4();
             persons.push(newPerson);
             res.send(persons)
         } else {
